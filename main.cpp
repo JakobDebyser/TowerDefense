706459e5bd42a388f0e5342613aa861c16378020
jakob debyser <jakob.debyser@hotmail.com>
2021-12-13 11:19:48 +0100

    map loaded

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
-
+#include"raylib.h"
 
 int main(){
-    
+    InitWindow(1024,768,"TowerDefense");
+   Texture2D WorldMap= LoadTexture("textures/testmap.png");
+
+
+    SetTargetFPS(60);
+    while(!WindowShouldClose()){
+        BeginDrawing();
+        ClearBackground(WHITE);
+        DrawTextureEx(WorldMap,{0,0},0.f,2,WHITE);
+
+
+
+        EndDrawing();
+    }
+
     return 0;
 }
